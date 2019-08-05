a7a44ea087be0d430d3c13f9b7fa81e77a858483
Shamiul Haque <samiul4413@gmail.com>
2019-08-06 00:17:15 +0600

    Initial Commit

---
+++ SmackChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmackChat
+//
+//  Created by Samiul Haque on 6/8/19.
+//  Copyright © 2019 Samiul Haque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
